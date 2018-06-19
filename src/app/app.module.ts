768ed91754ac7f85256540e0a0b299de9d1666b7
Shannon Reimer <seasquawks@hotmail.com>
2018-06-19 17:59:43 -0600

    Adding an inital venue component, and updating app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { VenueComponent } from './venue/venue.component';
 
+const routes: Routes = [
+  { path: 'venue', component: VenueComponent, data: { title: 'venue' } }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VenueComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
